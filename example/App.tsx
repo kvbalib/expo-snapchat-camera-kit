0d13f655a6f26d1d040c5a56a5c25929d979a15e
Kuba <zero@fucks.gives>
2023-03-02 22:42:30 +0100

    initial project setup

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import * as ExpoSnapchatCameraKit from 'expo-snapchat-camera-kit';
+import * as ExpoSnapchatCameraKit from "expo-snapchat-camera-kit";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{ExpoSnapchatCameraKit.hello()}</Text>
+      <Text>{ExpoSnapchatCameraKit.getSnapchatKitAppID()}</Text>
+      <Text>{ExpoSnapchatCameraKit.getSnapchatApiToken()}</Text>
+      <Text>{ExpoSnapchatCameraKit.getSnapchatLensGroupID()}</Text>
     </View>
   );
 }
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
